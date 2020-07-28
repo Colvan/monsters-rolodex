b67a97a9dd4ced7740796a9866e4d9ab81f7dd24
Colvan <yoniborisenco@gmail.com>
2020-07-28 13:39:18 +0300

    Updates

---
+++ precache-manifest.286a6e1d07b159bb1d9778bd827674f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "932856a752a32e5eb912bb4f3645dfdf",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "31dde52d2b2941bbeb75",
+    "url": "/monsters-rolodex/static/css/main.e14b445d.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31dde52d2b2941bbeb75",
+    "url": "/monsters-rolodex/static/js/main.f5bc2eec.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
